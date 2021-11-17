c3a1f26926f64886f8bfee75447edbaa39b33a2a
Iñaki Arango <arangoinaki@gmail.com>
2021-11-17 17:42:11 -0600

    First updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,17 +3,17 @@
  *
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
-require("dotenv").config({
+require('dotenv').config({
   path: `.env.${process.env.NODE_ENV}`,
-})
+});
 module.exports = {
   siteMetadata: {
-    title: "Barcadia",
-    description: "A super-fast site using GatsbyJS",
-    author: "Morgan Baker",
-    twitterUsername: "@dave",
-    image: "/yellow-metal-design-decoration.jpg",
-    siteUrl: "https://barcadia.netlify.com",
+    title: 'Rice Undergraduate Sustainability Fund',
+    description: 'A super-fast site using GatsbyJS',
+    author: 'Iñaki Arango',
+    twitterUsername: '@inaki_arango',
+    image: '/yellow-metal-design-decoration.jpg',
+    siteUrl: 'https://barcadia.netlify.com',
   },
   /* Your site config here */
   plugins: [
@@ -42,11 +42,11 @@ module.exports = {
       },
     },
     {
-      resolve: "gatsby-plugin-robots-txt",
+      resolve: 'gatsby-plugin-robots-txt',
       options: {
-        host: "https://barcadia.netlify.com",
-        sitemap: "https://barcadia.netlify.com/sitemap.xml",
-        policy: [{ userAgent: "*", allow: "/" }],
+        host: 'https://barcadia.netlify.com',
+        sitemap: 'https://barcadia.netlify.com/sitemap.xml',
+        policy: [{ userAgent: '*', allow: '/' }],
       },
     },
     `gatsby-plugin-playground`,
@@ -56,4 +56,4 @@ module.exports = {
     `gatsby-plugin-image`,
     `gatsby-plugin-transition-link`,
   ],
-}
+};
