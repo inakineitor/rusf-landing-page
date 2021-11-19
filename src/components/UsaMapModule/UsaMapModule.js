09644135a6316afbf48f76f25dc56a91d3f82910
Iñaki Arango <arangoinaki@gmail.com>
2021-11-19 01:32:10 -0600

    Changed situated to located

--- src/components/UsaMapModule/UsaMapModule.js
+++ src/components/UsaMapModule/UsaMapModule.js
@@ -15,7 +15,7 @@ const UsaMapModule = () => {
               We are at the heart of the Energy Revolution
             </h2>
             <p style={{ marginBottom: '60px' }}>
-              Situated in Houston, Texas, Rice University, has historically generated the leaders of energy finance. We have a collection of experienced advisors across the entire capital stack.
+              Located in Houston, Texas, Rice University, has historically generated the leaders of energy finance. We have a collection of experienced advisors across the entire capital stack.
             </p>
           </div>
           <img className="map" src={usaMap} alt="Houston in USA Map" />
